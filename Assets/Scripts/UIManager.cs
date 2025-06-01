f1d40fadb10f761a1a6630ab429a6b31971f1ccc
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2025-06-01 19:21:53 +0100

    Basic package and UI setup

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void Electric()
+    {
+        Debug.Log("Electric");
+    }
+
+    public void Water()
+    {
+        Debug.Log("Water");
+    }
+
+    public void Air()
+    {
+        Debug.Log("Air");
+    }
+
+    public void Fire()
+    {
+        Debug.Log("Fire");
+    }
+
+    public void Earth()
+    {
+        Debug.Log("Earth");
+    }
+}
